d09d869907ee9e96a92f4d594a5182876aea2517
Orwa400 <matheworwa3@gmail.com>
2025-01-06 23:59:30 +0300

    Add Age distr and country charts

--- virtual/app.py
+++ virtual/app.py
@@ -1,6 +1,6 @@
-import streanlit as st
-import pandas as pd
-import plotly.express as px
+import streanlit as st # Create web app with python
+import pandas as pd # For data manipulation and analysis
+import plotly.express as px # Create interactive visualizations
 
 # load data
 @st.cache
@@ -19,9 +19,28 @@ selected_country = st.sidebar.multiselect("Country", data["Country"].unique())
 selected_gender = st.sidebar.multiselect("Gender", data["Gender"].unique())
 
 # Apply Filters
-filtered_data = data.copy()
+filtered_data = data.copy() # Start with the copy of the original dataset
 if selected_country:
     filtered_data = filtered_data[filtered_data["Country"].isin(selected_country)]
 if selected_gender:
     filtered_data = filtered_data[filtered_data["Gender"].isin(selected_gender)]
 
+#Display filtered Data
+st.subheader("filtered Data") # This section creates a subheader for filtered data and displays the `filtered_data` DataFrame as an interactive table in the Streamlit app.
+st.subheader("Filtered Data")  
+st.dataframe(filtered_data)
+
+# Age Distribution
+# This section generates a histogram to visualize the distribution of customer ages in the filtered dataset.
+st.markdown("### Age distribution")
+fig_age = px.histogram(filtered_data, x="Age", nbins=10, title="Age Distribution")
+st.plotly_chart(fig_age)  # Display the Plotly chart in the Streamlit app.
+
+# Purchase amount by country
+# This section generates a bar chart to show the total purchase amount for each country in the filtered dataset.
+st.markdown("### Purchsse Amount by Country")
+fig_purchase = px.bar(
+    filtered_data.groupby("Country")["PurchaseAmount"].sum().reset_index(name="Signups"),
+    x="Country", y="Total Purchase Amount", title="Purchase Amount by Country"
+)
+st.plotly_chart(fig_purchase) # Display the Plotly bar chart in the Streamlit app.
